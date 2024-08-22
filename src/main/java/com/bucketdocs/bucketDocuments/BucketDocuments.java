163f21244b2956925ed3ee4079b38d1fd3f2f262
sall0-0p <antoniuk.orest@gmail.com>
2024-08-22 11:48:12 +0200

    First commit

--- src/main/java/com/bucketdocs/bucketDocuments/BucketDocuments.java
+++ src/main/java/com/bucketdocs/bucketDocuments/BucketDocuments.java
@@ -2,8 +2,6 @@ package com.bucketdocs.bucketDocuments;
 
 import org.bukkit.plugin.java.JavaPlugin;
 
-import java.util.logging.Logger;
-
 public final class BucketDocuments extends JavaPlugin {
     private static BucketDocuments plugin;
 
